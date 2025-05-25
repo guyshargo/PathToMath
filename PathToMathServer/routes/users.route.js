b834ccd1b97c39e06d02d3a3a3eb01d94c717b1a
YardenNahum <yardennahum2@gmail.com>
2025-05-25 13:43:18 +0300

    Created server side and database connection

---
+++ PathToMathServer/routes/users.route.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const usersRouter = express.Router();
+const User = require('../models/userSchema');
+
+//GET request for all users
+usersRouter.get("/", async (req,res)=>{
+    let users = await User.find({},{_id:0});
+    res.status(200).send(users);
+});
+
+//GET request for user by email
+usersRouter.get("/:email", async (req,res)=>{
+    let users = await User.find({name: req.params.email});
+    res.status(200).send(users);
+});
+//POST request to add new user
+usersRouter.post("/register", async (req,res)=>{
+    let user = new User(req.body);
+    await user.save();
+    res.status(200).send(user);
+});
+
+//PUT request to update all user details by email
+usersRouter.put("/update/:email", async (req,res)=>{
+    let user = await User.updateOne({email: req.params.email},
+    {$set: req.body});
+    res.status(200).send(user);
+});
+
+module.exports = usersRouter;
