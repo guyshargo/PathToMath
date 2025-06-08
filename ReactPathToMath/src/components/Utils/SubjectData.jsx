539d6a887465a9c6f9f439ea2f1a5af7c3306cd9
YardenNahum <yardennahum2@gmail.com>
2025-06-08 13:25:11 +0300

    Video page fix

---
+++ ReactPathToMath/src/components/Utils/SubjectData.jsx
@@ -0,0 +1,28 @@
+import addition from '../../assets/Images/Math_icon/addition_purple.png';
+import subtraction from '../../assets/Images/Math_icon/minus.png';
+import multiplication from '../../assets/Images/Math_icon/multi.png';
+import division from '../../assets/Images/Math_icon/division1.png';
+import percentage from '../../assets/Images/Math_icon/percentage.png';
+
+export const subjectsData = {
+    Addition: {
+        icon: addition,
+        color: '#E0BBE4',
+    },
+    Subtraction: {
+        icon: subtraction,
+        color: '#FFABAB',
+    },
+    Multiplication: {
+        icon: multiplication,
+        color: '#B5EAD7',
+    },
+    Division: {
+        icon: division,
+        color: '#C7CEEA',
+    },
+    Percentage: {
+        icon: percentage,
+        color: '#FFDAC1',
+    },
+};
